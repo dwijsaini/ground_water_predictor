9840a5e1edeca6a0648fd93313505a36c9e088ee
dwijsaini <dwijsaini2@gmail.com>
2026-09-19 22:03:30 +0530

    Build terminal navigation

---
+++ src/terminal.js
@@ -0,0 +1,59 @@
+const readline = require('readline');
+
+class TerminalUI {
+    constructor() {
+        this.currentOption = 0;
+    }
+
+    clear() {
+        process.stdout.write('\x1Bc');
+    }
+
+    drawBox(title, options, selectedIndex) {
+        const width = 48;
+        const border = '═'.repeat(width - 2);
+
+        console.log(`╔${border}╗`);
+        console.log(`║${title.padStart((width + title.length) / 2).padEnd(width - 2)}║`);
+        console.log(`╠${border}╣`);
+        console.log(`║${' '.repeat(width - 2)}║`);
+
+        options.forEach((option, index) => {
+            const prefix = index === selectedIndex ? '> ' : '  ';
+            const text = `${prefix}${option}`;
+            const padding = ' '.repeat(width - 2 - text.length);
+            console.log(`║${text}${padding}║`);
+        });
+
+        console.log(`║${' '.repeat(width - 2)}║`);
+        console.log(`╚${border}╝`);
+    }
+
+    async prompt(question) {
+        return new Promise(resolve => {
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
+            rl.question(question, (answer) => {
+                rl.close();
+                resolve(answer);
+            });
+        });
+    }
+
+    async waitForKey() {
+        return new Promise(resolve => {
+            process.stdin.once('data', data => {
+                resolve(data);
+            });
+        });
+    }
+
+    pause() {
+        console.log('\nPress any key to return to menu...');
+        return this.waitForKey();
+    }
+}
+
+module.exports = TerminalUI;
